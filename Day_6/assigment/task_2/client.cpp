b2c8cbfda644ca2c19754b33a5f3b0374313e7a1
Rohit Gite <rohitgite1419@gmail.com>
2026-09-02 14:53:30 +0530

    remove retaking tenure in FD

--- Day_6/assigment/task_2/client.cpp
+++ Day_6/assigment/task_2/client.cpp
@@ -2,111 +2,118 @@
 #include "current_acc.h"
 #include "fixed_deposit.h"
 
-
-class PrintDetails {
-    public: 
-
-    static void details(Account* aptr){
+class PrintDetails
+{
+public:
+    static void details(Account *aptr)
+    {
 
         aptr->displayAccInfo();
 
         aptr->caluculate_returns();
 
-        if(typeid(*aptr) == typeid(Saving)){
-            Saving* sptr ; 
-            sptr = dynamic_cast<Saving*>(aptr);
+        if (typeid(*aptr) == typeid(Saving))
+        {
+            Saving *sptr;
+            sptr = dynamic_cast<Saving *>(aptr);
             sptr->saving_annocement();
         }
-        else if(typeid(*aptr) == typeid(Current)){
-            Current* cptr ; 
-            cptr = dynamic_cast<Current*>(aptr);
+        else if (typeid(*aptr) == typeid(Current))
+        {
+            Current *cptr;
+            cptr = dynamic_cast<Current *>(aptr);
             cptr->curr_announcement();
         }
-         else {
-            FD_Account* cptr ; 
-            cptr = dynamic_cast<FD_Account*>(aptr);
+        else
+        {
+            FD_Account *cptr;
+            cptr = dynamic_cast<FD_Account *>(aptr);
             cptr->fd_announcement();
         }
-
     }
-
 };
 
-int main () {
+int main()
+{
 
-    Account** acc_arr; 
-    int noa; 
-    cout<<"\n Enter number of account u want to create :"; 
-    cin>>noa;
+    Account **acc_arr;
+    int noa;
+    cout << "\n Enter number of account u want to create :";
+    cin >> noa;
 
-    acc_arr = new Account* [noa];
+    acc_arr = new Account *[noa];
 
-    for(int i = 0; i < noa; i ++) {
-        int choice; 
-        cout <<"\n 1. Saving Acc \n 2. Curruent Acc \n 3. Fixed Deposit " ; 
-        cin>>choice;
+    for (int i = 0; i < noa; i++)
+    {
+        int choice;
+        cout << "\n 1. Saving Acc \n 2. Curruent Acc \n 3. Fixed Deposit \n ";
+        cout << "Enter choice : ";
+        cin >> choice;
 
-        switch (choice){
+        switch (choice)
+        {
 
-            case(1):{ 
-            string name; 
+        case (1):
+        {
+            string name;
             float balance;
-            cout << "\n Saving Account============"; 
-            cout << "\n enter Customer name : "; 
-            cin>>name;
-            cout << "\n enter Depsit ammount : "; 
-            cin>>balance;
+            cout << "\n Saving Account============";
+            cout << "\n enter Customer name : ";
+            cin >> name;
+            cout << "\n enter Depsit ammount : ";
+            cin >> balance;
 
-            acc_arr[i] = new Saving(name, balance ) ;
+            acc_arr[i] = new Saving(name, balance);
 
             break;
-            }
+        }
 
-            case(2):{
-                string name; 
+        case (2):
+        {
+            string name;
             float balance;
-                cout << "\n Current Account============"; 
-            cout << "\n enter Customer name : "; 
-            cin>>name;
-            cout << "\n enter Deposit ammount : "; 
-            cin>>balance;
+            cout << "\n Current Account============";
+            cout << "\n enter Customer name : ";
+            cin >> name;
+            cout << "\n enter Deposit ammount : ";
+            cin >> balance;
 
-            acc_arr[i] = new Current(name, balance ) ;
+            acc_arr[i] = new Current(name, balance);
 
             break;
-            }
+        }
 
-            case(3):{
-            string name; 
+        case (3):
+        {
+            string name;
             float balance;
-            float tenure ;
-                cout << "\n Fixed Deposit============"; 
-            cout << "\n enter Customer name : "; 
-            cin>>name;
-            cout << "\n enter Depsit balance : "; 
-            cin>>balance;
-            cout << "\n enter investment tenure : "; 
-            cin>>tenure;
 
-            acc_arr[i] = new FD_Account(name, balance, tenure ) ;
+            cout << "\n Fixed Deposit============";
+            cout << "\n enter Customer name : ";
+            cin >> name;
+            cout << "\n enter Depsit balance : ";
+            cin >> balance;
+
+            acc_arr[i] = new FD_Account(name, balance);
 
             break;
-            }
+        }
         }
     }
 
-    for(int i = 0 ; i < noa; i ++){
+    for (int i = 0; i < noa; i++)
+    {
         PrintDetails::details(acc_arr[i]);
-
     }
 
-    // clear memory 
+    // clear memory
 
-    for(int i = 0; i < noa; i++){
+    for (int i = 0; i < noa; i++)
+    {
         delete acc_arr[i];
     }
 
     delete[] acc_arr;
 
-    return 0 ; 
+    return 0;
 }
--- Day_6/assigment/task_2/fixed_deposit.h
+++ Day_6/assigment/task_2/fixed_deposit.h
@@ -1,43 +1,41 @@
 #pragma once
-#include"account.h"
+#include "account.h"
 
-class FD_Account: public Account{
-    private:
-    static float int_rate; 
-     float tenure;
+class FD_Account : public Account
+{
+private:
+  static float int_rate;
+  float tenure;
 
-   public:
-
-   FD_Account(string name, float balance, float tenure):Account(name, balance){
-    cout<< "\n Select your tenure eg.: 1, 3, 5 years : "; 
-    cin>>tenure;
+public:
+  FD_Account(string name, float balance) : Account(name, balance)
+  {
+    cout << "\n Select your tenure eg.: 1, 3, 5 years : ";
+    cin >> tenure;
 
     this->tenure = tenure;
+  }
 
-   }
-
-   void displayAccInfo(){
+  void displayAccInfo()
+  {
     Account::displayAccInfo();
-    cout<<"\n int_rate: " << int_rate;
-    cout<< "\n Principle: " << balance;
-    cout<< "\n FD Tenure: " << tenure;
-   }
+    cout << "\n int_rate: " << int_rate;
+    cout << "\n Principle: " << balance;
+    cout << "\n FD Tenure: " << tenure;
+  }
 
+  float caluculate_returns()
+  {
 
-   float caluculate_returns(){
-
-    float maturity_amt = (int_rate * balance * tenure) ; 
+    float maturity_amt = (int_rate * balance * tenure);
 
     return maturity_amt;
+  }
 
-   }
-
-   void fd_announcement(){
-    cout<<"Dont brake your FD before maturity; otherwise charges will be applied";
-   }
-
-
+  void fd_announcement()
+  {
+    cout << "Dont brake your FD before maturity; otherwise charges will be applied";
+  }
 };
 
 float FD_Account::int_rate = 0.08;
-
